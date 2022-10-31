f80257cc83e19a8d53155e33a40ed87b682f75e4
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-10-31 15:32:25 +0100

    feat: Created controlled form that updates state onChange
    When user types data in the textarea the state of the object gets updated and returns the state to the textarea as value.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import './App.css'
 import React, { useState } from 'react'
 
 function App() {
-
+  const [text, setText] = useState("")
+  
+  function handleChange(event){
+    setText(event.target.value)
+  }
+  
   return (
     <>
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea 
+        name='text'
+        onChange={handleChange}
+        placeholder="Type your text here"
+        value={text}
+      />
       <h4>Time remaining: ...</h4>
       <button>Start</button>
       <h2>Word count: ...</h2>
