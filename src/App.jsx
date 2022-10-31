6daa5545c6d0d0bfb05aa50da4a242cbe738ee17
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-10-31 17:09:14 +0100

    feat: Timer set on start and word count displays on 0
    Timer starts automatically with countdown when clicking on start button and words are counted and displayed when counter hits 0

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,34 @@ import React, { useEffect, useState } from 'react'
 function App() {
   const [text, setText] = useState("")
   const [wordCountText, setwordCountText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [isRunning, setIsRunning] = useState(false)
+  const [timeRemaining, setTimeRemaining] = useState(0)
   
   useEffect(() => {
     if (timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining((prevTimeRemaining => prevTimeRemaining - 1))
       }, 1000)
-    } 
+    } else if (timeRemaining === 0){
+      showWordCount()
+      setIsRunning(false)
+    }
   },[timeRemaining])
 
   function handleChange(event){
     setText(event.target.value)
+  }
+
+  function startGame(){
+    if (!isRunning) {
+      setIsRunning(true)
+      setTimeRemaining(10)
+    }
+  }
+
+  async function showWordCount(){
+    const amountOfWords = await calculateWordCount()
+    setwordCountText(`Word count: ${amountOfWords}`)
   }
 
   function calculateWordCount(){
@@ -25,11 +41,6 @@ function App() {
       return wordsArray.length
     }
     return 0
-  }
-
-  async function showWordCount(){
-    const amountOfWords = await calculateWordCount()
-    setwordCountText(`Word count: ${amountOfWords}`)
   }
 
   return (
@@ -42,7 +53,7 @@ function App() {
         value={text}
       />
       <h4>Time remaining: {timeRemaining}s</h4>
-      <button onClick={showWordCount}>Start</button>
+      <button onClick={startGame}>Start</button>
       <h2 id="wordCount">{wordCountText}</h2>
     </>
   )
