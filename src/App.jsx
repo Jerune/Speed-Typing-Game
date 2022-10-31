5d58f8999422ca5e62afb1ed04670d7f95d3607a
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-10-31 16:51:29 +0100

    feat: Automatic countdown for Time Remaining
    Time remaining now has a state and automatically counts down until 0 using useEffect with setTimeout function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+// @ts-nocheck
 import './App.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
   const [text, setText] = useState("")
+  const [wordCountText, setwordCountText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(5)
   
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTimeRemaining => prevTimeRemaining - 1))
+      }, 1000)
+    } 
+  },[timeRemaining])
+
   function handleChange(event){
     setText(event.target.value)
   }
-  
+
+  function calculateWordCount(){
+    if(text.length > 0) {
+      const wordsArray = text.trim().split(" ")
+      return wordsArray.length
+    }
+    return 0
+  }
+
+  async function showWordCount(){
+    const amountOfWords = await calculateWordCount()
+    setwordCountText(`Word count: ${amountOfWords}`)
+  }
+
   return (
     <>
       <h1>How fast do you type?</h1>
@@ -17,9 +41,9 @@ function App() {
         placeholder="Type your text here"
         value={text}
       />
-      <h4>Time remaining: ...</h4>
-      <button>Start</button>
-      <h2>Word count: ...</h2>
+      <h4>Time remaining: {timeRemaining}s</h4>
+      <button onClick={showWordCount}>Start</button>
+      <h2 id="wordCount">{wordCountText}</h2>
     </>
   )
 }
