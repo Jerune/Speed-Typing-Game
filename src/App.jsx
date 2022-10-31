f29a9d21c6fd858389dca07819a9f8cb77c5c636
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-10-31 15:19:17 +0100

    feat: Set App Base Structure + Styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import React, { useState } from 'react'
 function App() {
 
   return (
-    <h1></h1>
+    <>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Time remaining: ...</h4>
+      <button>Start</button>
+      <h2>Word count: ...</h2>
+    </>
   )
 }
 
